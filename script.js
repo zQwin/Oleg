16e4eb43a99167692ffcd33e66c12f03adf5adca
Oleg <mitinom98@gmail.com>
2023-09-22 12:54:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const answer = prompt("Вам есть 18?", " ");
+// console.log(answer);
+
+const answers = [];
+
+// answers[0] = prompt("как ваше имя ?", "");
+// answers[1] = prompt("как вашa фамилия ?", "");
+// answers[2] = prompt("Сколько вам лет  ?", "");
+
+console.log(typeof answers);
+console.log(typeof null);
